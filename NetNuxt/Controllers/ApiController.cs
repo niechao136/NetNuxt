fee6c8774c33b1cc3fc898f836147b4c5aead6ee
nie.chao <nie.chao@advantech.com.cn>
2023-02-15 18:04:18 +0800

    controller

--- NetNuxt/Controllers/ApiController.cs
+++ NetNuxt/Controllers/ApiController.cs
@@ -1,12 +1,108 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using NetNuxt.Middlewares;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using NetNuxt.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace NetNuxt.Controllers;
 
-public class ApiController : Controller, CallAPI
+public class ApiController : BaseController
 {
-    public string post()
+    private readonly IOptions<AppSettings> _settings;
+
+    public ApiController(IOptions<AppSettings> settings)
     {
-        return "";
+        _settings = settings;
+    }
+
+    public string Post([FromBody]JObject data)
+    {
+        var url = data["url"]?.ToString()!;
+        if (!NOT_NEED_TOKEN.Contains(url) && HttpContext.User.Claims.Any())
+        {
+            return new JObject(new JProperty("status", 2)).ToString();
+        }
+        var form = data["data"];
+        var result = PostRequest(UrlHandler(url), DataHandler(url, form!));
+        switch (url)
+        {
+            case "login":
+                Login(result);
+                break;
+            case "logout":
+                Logout(result);
+                break;
+        }
+        return result;
+    }
+    
+    private static readonly string[] POS_URL = { "pos/query", "pos/isposdata", "pos/upload" };
+    private string UrlHandler(string url)
+    {
+        return POS_URL.Contains(url)
+            ? _settings.Value.POSAPIPATH?.URL + "/api/" + url
+            : _settings.Value.APIPATH?.URL + "/api/" + url;
+    }
+
+    private static readonly string[] NOT_NEED_TOKEN = { "" };
+    private string DataHandler(string url, JToken data)
+    {
+        if (NOT_NEED_TOKEN.Contains(url)) return data.ToString();
+        var form = new JObject(data);
+        var token = JsonConvert.DeserializeObject<JObject>(HttpContext.User.Claims
+            .SingleOrDefault(x => x.Type == "token")?.Value!);
+        if (POS_URL.Contains(url))
+        {
+            var pos = new JObject
+            {
+                new JProperty("token", token)
+            };
+            form.Add(new JProperty("token", pos));
+            return form.ToString();
+        }
+        form.Add(new JProperty("token", token));
+        if (form.ContainsKey("password"))
+        {
+            var psw = form["password"]?.ToString()!;
+            form["password"] = MD5Encrypt(psw);
+        }
+        return form.ToString();
+    }
+
+    private void Login(string result)
+    {
+        var res = JsonConvert.DeserializeObject<JObject>(result);
+        if (res?["status"]?.ToString() == "1")
+        {
+            var claims = new[]
+            {
+                new Claim("user_id",res["user_id"]?.ToString()!),
+                new Claim("token",res["token"]?.ToString()!),
+                new Claim("third_party",result),
+            };
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var authProperties = new AuthenticationProperties
+            {
+                IsPersistent = false,
+                ExpiresUtc = DateTimeOffset.UtcNow.AddDays(1), // 过期时间 1天
+                AllowRefresh = true // 如果 AllowRefresh 为 false，不延长 cookie 在客户端计算机硬盘上的保留时间，时间到了客户端计算机就自动删除 cookie
+            };
+            HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties).Wait();
+        }
+    }
+
+    private void Logout(string result)
+    {
+        var res = JsonConvert.DeserializeObject<JObject>(result);
+        if (res?["status"]?.ToString() == "1")
+        {
+            HttpContext.SignOutAsync().Wait();
+        }
     }
 }
---
+++ NetNuxt/Controllers/BaseController.cs
@@ -0,0 +1,139 @@
+﻿using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetNuxt.Controllers;
+
+public class BaseController : Controller
+{
+    #region Call API
+
+    private static readonly HttpClient client = httpClient();
+
+    private static HttpClient httpClient()
+    {
+        HttpClientHandler handler = new HttpClientHandler
+        {
+            ClientCertificateOptions = ClientCertificateOption.Manual,
+            ServerCertificateCustomValidationCallback = (httpRequestMessage, cert, cetChain, policyErrors) => true
+        };
+        HttpClient httpClient = new HttpClient(handler);
+        //表头参数
+        httpClient.DefaultRequestHeaders.Accept.Clear();
+        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        httpClient.Timeout = TimeSpan.FromSeconds(600);
+
+        return httpClient;
+    }
+
+    protected static string PostRequest(string url, string data)
+    {
+        HttpContent httpContent = new StringContent(data);
+        var task = client.PostAsync(url, httpContent);
+        task.Wait();
+        if (task.Result.IsSuccessStatusCode)
+        {
+            var read = task.Result.Content.ReadAsStringAsync();
+            read.Wait();
+            return read.Result;
+        }
+
+        return "";
+    }
+
+    #endregion Call API
+
+    #region 加密解密
+
+    /// <summary>
+    /// 128位处理key
+    /// </summary>
+    /// <param name="keyArray">原字节</param>
+    /// <param name="key">处理key</param>
+    /// <returns></returns>
+    private static byte[] GetAesKey(IReadOnlyList<byte> keyArray, string key)
+    {
+        byte[] newArray = new byte[16];
+        //if (keyArray.Length < 16)
+        //{
+        for (int i = 0; i < newArray.Length; i++)
+        {
+            if (i >= keyArray.Count)
+            {
+                newArray[i] = 0;
+            }
+            else
+            {
+                newArray[i] = keyArray[i];
+            }
+        }
+
+        //}
+        return newArray;
+    }
+
+    /// <summary>
+    /// 使用AES加密字符串,按128位处理key
+    /// </summary>
+    /// <param name="content">加密内容</param>
+    /// <param name="key">秘钥，需要128位、256位.....</param>
+    /// <param name="autoHandle"></param>
+    /// <returns>Base64字符串结果</returns>
+    protected static string AesEncrypt(string content, string key, bool autoHandle = true)
+    {
+        byte[] keyArray = Encoding.UTF8.GetBytes(key);
+        if (autoHandle)
+        {
+            keyArray = GetAesKey(keyArray, key);
+        }
+
+        byte[] toEncryptArray = Encoding.UTF8.GetBytes(content);
+
+        SymmetricAlgorithm des = Aes.Create();
+        des.Key = keyArray;
+        des.Mode = CipherMode.ECB;
+        des.Padding = PaddingMode.PKCS7;
+        ICryptoTransform cTransform = des.CreateEncryptor();
+        byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+        return Convert.ToBase64String(resultArray);
+    }
+
+    /// <summary>
+    /// 使用AES解密字符串,按128位处理key
+    /// </summary>
+    /// <param name="content">内容</param>
+    /// <param name="key">秘钥，需要128位、256位.....</param>
+    /// <param name="autoHandle"></param>
+    /// <returns>UTF8解密结果</returns>
+    protected static string AesDecrypt(string content, string key, bool autoHandle = true)
+    {
+        byte[] keyArray = Encoding.UTF8.GetBytes(key);
+        if (autoHandle)
+        {
+            keyArray = GetAesKey(keyArray, key);
+        }
+
+        byte[] toEncryptArray = Convert.FromBase64String(content);
+
+        SymmetricAlgorithm des = Aes.Create();
+        des.Key = keyArray;
+        des.Mode = CipherMode.ECB;
+        des.Padding = PaddingMode.PKCS7;
+
+        ICryptoTransform cTransform = des.CreateDecryptor();
+        byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+        return Encoding.UTF8.GetString(resultArray);
+    }
+
+    protected static string MD5Encrypt(string content)
+    {
+        var md5_byte = MD5.HashData(Encoding.UTF8.GetBytes(content));
+        var md5_str = BitConverter.ToString(md5_byte).ToLower();
+        return md5_str.Replace("-", "");
+    }
+
+    #endregion 加密解密
+}
